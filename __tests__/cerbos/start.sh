1195d2a4e8a3c053ffdc8a64b7ca64e9d8de4b5c
Andrew Haines <haines@cerbos.dev>
2022-02-22 15:17:20 +0000

    chore(ci): Run tests in CI (#10)
    Signed-off-by: Andrew Haines <haines@cerbos.dev>

--- __tests__/cerbos/start.sh
+++ __tests__/cerbos/start.sh
@@ -1,6 +1,14 @@
-docker run -i -t -p 8080:8080 \
-  -v $(pwd)/config:/config \
-  -v $(pwd)/keys:/keys \
-  -v $(pwd)/policies:/policies \
-  ghcr.io/cerbos/cerbos:0.11.0 \
-  server --config=/config/conf.yaml
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+docker run \
+  --interactive \
+  --publish 8080:8080 \
+  --mount type=bind,source="${PWD}/config",target=/config,readonly \
+  --mount type=bind,source="${PWD}/keys",target=/keys,readonly \
+  --mount type=bind,source="${PWD}/policies",target=/policies,readonly \
+  ghcr.io/cerbos/cerbos:latest \
+  server \
+  --config /config/conf.yaml
