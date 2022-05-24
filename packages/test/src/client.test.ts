58c0b76fe704db6e8309819ef5a75c8e11e1dd22
Andrew Haines <haines@cerbos.dev>
2022-05-24 15:30:08 +0100

    Set up tests
    Signed-off-by: Andrew Haines <haines@cerbos.dev>

---
+++ packages/test/src/client.test.ts
@@ -0,0 +1,41 @@
+import { Client } from "@cerbos/core";
+import { GRPC } from "@cerbos/grpc";
+import { HTTP } from "@cerbos/http";
+import { beforeAll, describe, expect, it } from "@jest/globals";
+
+import { Ports, cerbosVersion, ports as serverPorts } from "./servers";
+
+describe("client", () => {
+  let ports: Ports;
+
+  beforeAll(async () => {
+    ports = await serverPorts();
+  });
+
+  const cases = [
+    {
+      type: "gRPC | TCP | plaintext",
+      client: (): Client => new GRPC(`localhost:${ports.grpc.plaintext}`),
+    },
+    {
+      type: "HTTP",
+      client: (): Client => new HTTP(`http://localhost:${ports.http}`),
+    },
+  ];
+
+  describe.each(cases)("$type", ({ client }: typeof cases[number]) => {
+    describe("serverInfo", () => {
+      it("returns information about the server", async () => {
+        const result = await client().serverInfo();
+
+        expect(result).toEqual({
+          buildDate: expect.stringMatching(
+            /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/
+          ) as unknown,
+          commit: expect.stringMatching(/^[0-9a-f]{40}$/) as unknown,
+          version: cerbosVersion,
+        });
+      });
+    });
+  });
+});
---
+++ packages/test/src/servers.ts
@@ -0,0 +1,74 @@
+import { execFile as execFileCallback } from "child_process";
+import { resolve } from "path";
+import { promisify } from "util";
+
+const execFile = promisify(execFileCallback);
+
+export const cerbosVersion = process.env.CERBOS_VERSION ?? "0.16.0";
+
+export interface Ports {
+  grpc: {
+    plaintext: number;
+    tls: number;
+    mtls: number;
+  };
+  http: number;
+}
+
+interface DockerComposeContainer {
+  Service: string;
+  Publishers:
+    | {
+        PublishedPort: number;
+        TargetPort: number;
+      }[]
+    | null;
+}
+
+export const ports = async (): Promise<Ports> => {
+  const { stdout } = await execFile("docker", [
+    "compose",
+    "--file",
+    resolve(__dirname, "../servers/docker-compose.yml"),
+    "ps",
+    "--format",
+    "json",
+  ]);
+
+  const output = JSON.parse(stdout) as DockerComposeContainer[];
+
+  return {
+    grpc: {
+      plaintext: port(output, "plaintext", 3593),
+      tls: port(output, "tls", 3593),
+      mtls: port(output, "mtls", 3593),
+    },
+    http: port(output, "plaintext", 3592),
+  };
+};
+
+const port = (
+  containers: DockerComposeContainer[],
+  service: string,
+  targetPort: number
+): number => {
+  const container = containers.find(
+    (container) => container.Service === service
+  );
+
+  if (!container) {
+    throw new Error(`Couldn't find container for ${service} service`);
+  }
+
+  const publisher = (container.Publishers ?? []).find(
+    (publisher) => publisher.TargetPort === targetPort
+  );
+
+  if (!publisher || publisher.PublishedPort === 0) {
+    throw new Error(
+      `Couldn't find published port ${targetPort} for ${service} service`
+    );
+  }
+
+  return publisher.PublishedPort;
+};
