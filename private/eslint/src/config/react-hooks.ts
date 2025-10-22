d86bc90d7b132525c189d1e2bb27c41fab9ccd67
Andrew Haines <haines@cerbos.dev>
2025-10-22 10:46:57 +0100

    Fix lint errors
    Signed-off-by: Andrew Haines <haines@cerbos.dev>

--- private/eslint/src/config/react-hooks.ts
+++ private/eslint/src/config/react-hooks.ts
@@ -4,5 +4,5 @@ import reactHooksPlugin from "eslint-plugin-react-hooks";
 export const reactHooksConfig = defineConfig({
   files: ["packages/react/**"],
   // @ts-expect-error -- https://github.com/facebook/react/issues/34801
-  extends: [reactHooksPlugin.configs.flat.recommended],
+  extends: [reactHooksPlugin.configs.flat.recommended], // eslint-disable-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access -- https://github.com/facebook/react/issues/34801
 });
