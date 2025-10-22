c104328e264b4602428afd1f6c9c5547f21a84a8
Andrew Haines <haines@cerbos.dev>
2025-10-22 11:02:25 +0100

    Fix type errors
    Signed-off-by: Andrew Haines <haines@cerbos.dev>

--- private/eslint/src/config/vitest.ts
+++ private/eslint/src/config/vitest.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "eslint/config";
 
 export const vitestConfig = defineConfig({
   files: ["**/*.test.ts"],
+  // @ts-expect-error -- https://github.com/vitest-dev/eslint-plugin-vitest/issues/761
   extends: [vitestPlugin.configs.recommended],
   settings: {
     vitest: {
